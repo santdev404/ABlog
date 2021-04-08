92a56a35a0b72ae3c2c8e07631a00a8c6d1fc0d2
Santiago Vargas <santdev404@gmail.com>
2021-04-08 09:14:44 -0500

    Admin fixes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,13 @@
 class ArticlesController < ApplicationController
 
     before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:index, :show]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def index
 
-        @articles = Article.all
+        # @articles = Article.all
+        @articles = Article.paginate(page: params[:page], per_page: 5)
 
     end
 
@@ -21,7 +24,7 @@ class ArticlesController < ApplicationController
     def create
         
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         
         if @article.save
 
@@ -69,6 +72,13 @@ class ArticlesController < ApplicationController
 
         params.require(:article).permit(:title, :description)
 
+    end
+
+    def require_same_user
+        if current_user != @article.user && !current_user.admin?
+            flash[:alert] = "You can only edit or delete your own article"
+            redirect_to article_path(@article)
+        end
     end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,9 @@ module ApplicationHelper
         image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
     end
 
+
+
+
+
+
 end
