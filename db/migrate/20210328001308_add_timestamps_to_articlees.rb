217c1e6c61bfccf79c243933e458c4d4928fb526
Santiago Vargas <santdev404@gmail.com>
2021-04-08 09:25:37 -0500

    DB fix

--- db/migrate/20210328001308_add_timestamps_to_articlees.rb
+++ db/migrate/20210328001308_add_timestamps_to_articlees.rb
@@ -1,4 +1,4 @@
-class AddTimestampsToArticlees < ActiveRecord::Migration
+class AddTimestampsToArticlees < ActiveRecord::Migration[5.2]
   def change
     add_column :articles, :created_at, :datetime
     add_column :articles, :updated_at, :datetime
